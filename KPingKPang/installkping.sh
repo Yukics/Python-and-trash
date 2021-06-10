0e5d947c1563e88e72d7b92a2fe388e669eb7b61
Yuki CS <34201397+Yukics@users.noreply.github.com>
2021-06-10 21:25:05 +0200

    Ya hecho

--- KPingKPang/installkping.sh
+++ KPingKPang/installkping.sh
@@ -1,1 +1,24 @@
-nohup `pwd`kpingkpang.py &
+#/bin/bash
+if $EUID != 0; then
+    echo "Se necesita sudo"
+    exit 0
+fi
+touch /etc/systemd/system/kpingkpang.service
+cat << EOF >> /etc/systemd/system/kpingkpang.service
+[Unit]
+Description=kpingkpang service by yukics.
+After=multi-user.target
+Conflicts=getty@tty1.service
+
+[Service]
+Type=simple
+ExecStart=/usr/bin/python3 `pwd`/kpingkpang.py
+StandardInput=tty-force
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable kpingkpang.service
+sudo systemctl start kpingkpang.service
