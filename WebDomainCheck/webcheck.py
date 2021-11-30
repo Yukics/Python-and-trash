d0d2881f3b65061b158ce920ef8e0c8ad8240a78
Alejo Morell <34201397+Yukics@users.noreply.github.com>
2021-11-30 21:11:43 +0100

    Update Script

--- WebDomainCheck/webcheck.py
+++ WebDomainCheck/webcheck.py
@@ -25,13 +25,18 @@ except:
     print("No se encuentra el fichero: " + fichero)
     sys.exit(1)
 
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+
 #Checks every domain response status code if 200 ok something else error
 for lin in lineas:
+    
     web = ("http://"+lin).replace('\n','')
     web_secure = ("https://" + lin).replace('\n', '')
 
     try:
-        response = requests.get(web, verify=False)
+        session = requests.Session()
+        session.trust_env = False
+        response = session.get(web, verify=False, headers=headers)
         if response.status_code == 200:
             print(web + ": OK")
         else:
@@ -40,11 +45,12 @@ for lin in lineas:
         print("Error Tocho! Probablemente no exista " + web)
 
     try:
-        response = requests.get(web_secure, verify=False)
+        session = requests.Session()
+        session.trust_env = False
+        response = session.get(web_secure, verify=False, headers=headers)
         if response.status_code == 200:
             print(web_secure + ": OK")
         else:
             print(web_secure + "Error: " + response.status_code)
     except:
         print("Error Tocho! Probablemente no exista " + web_secure)
-        
